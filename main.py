c42a3f0c91aa7241e2c6f23d04c8838349a5efb9
as2sp <as2sp@aol.com>
2024-03-11 13:21:10 +0500

    Added show_password setting that allow to hide real password in app window. Rename show_pass setting to show_pass_in_log and moved it to [security] section. Added comments in code. Some other small fixes.

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ import logging
 
 
 def process_config_file():
+    # Processing special chars list in config.ini
     with open('config.ini', 'r') as file:
+
         for line in file:
+
             if line.startswith('special_symbols'):
                 special_symbols_str = line.split('=')[1].strip()
                 special_symbols_list = list(special_symbols_str)
@@ -17,8 +20,8 @@ def process_config_file():
     config = configparser.ConfigParser()
     config.read('config.ini')
 
+    # Processing common chars list in config.ini
     symbols = config.get('chars', 'symbols')
-
     if symbols == 'letters':
         characters = string.ascii_lowercase
     elif symbols == 'LETTERS':
@@ -30,6 +33,7 @@ def process_config_file():
         result = ''
 
         for symbol in symbol_list:
+
             if symbol == 'letters':
                 result += string.ascii_lowercase
             elif symbol == 'LETTERS':
@@ -39,35 +43,43 @@ def process_config_file():
             else:
                 result += symbol
         characters = result
+
     else:
         characters = symbols
 
+    # Check that logging is enabled in config.ini
     enable_logging = config.getboolean('log', 'enable')
     if enable_logging:
         log_filename = config.get('log', 'filename')
 
+    # Check that the minimum number of common chars between special chars is between 0 and 4
     if min_char_distance < 0 or min_char_distance > 4:
         print("min_char_distance must be between 0 and 4 in config.ini. Program terminated.")
+
         if enable_logging:
             logging.basicConfig(filename=log_filename, level=logging.INFO, format='[%(asctime)s] %(message)s',
                                 datefmt='%Y-%m-%d %H:%M:%S')
-            logging.info("min_char_distance more than 4 in config.ini. Program terminated.")
+            logging.info("min_char_distance less than 0 or more than 4 in config.ini. Program terminated.")
         raise SystemExit
 
     return special_symbols_list, min_char_distance, characters, enable_logging, config
 
 
 def write_to_log(password_length, clipboard_result, password, config):
+    # Start logging if enabled in config.ini
     enable_logging = config.getboolean('log', 'enable')
+
     if enable_logging:
         log_filename = config.get('log', 'filename')
+        print()
         print(f"Writing this event to log {log_filename}.")
-        show_pass = config.getboolean('log', 'show_pass')
+        show_pass_in_log = config.getboolean('security', 'show_pass_in_log')
 
         logging.basicConfig(filename=log_filename, level=logging.INFO, format='[%(asctime)s] %(message)s',
                             datefmt='%Y-%m-%d %H:%M:%S')
 
-        if show_pass:
+        # show_password setting logging here
+        if show_pass_in_log:
             logging.info(
                 f"Password length is {password_length} chars, {clipboard_result}. Generated password: {password}")
         else:
@@ -81,6 +93,7 @@ def main():
     while True:
         try:
             password_length = int(input("Enter the number (not less than 5) of password characters: "))
+
             if password_length > 4:
                 special_char_count = password_length // (random.randint(8, 10)) + 1
                 min_distance = min_char_distance + 1
@@ -101,26 +114,42 @@ def main():
                     else:
                         password += random.choice(characters)
 
-                print("Your password:", password)
-                print()
+                # Check show_password setting
+                if config.getboolean('security', 'show_password'):
+                    print()
+                    print("Your password:", password)
+                    print()
+                    copy_to_clipboard = input("Do you want to copy the result to the clipboard? (y/n): ")
 
-                copy_to_clipboard = input("Do you want to copy the result to the clipboard? (y/n): ")
+                    # Process copy-to-buffer action
+                    if copy_to_clipboard.lower() == "y":
+                        pyperclip.copy(str(password))
+                        clipboard_result = "and it has been copied to the clipboard"
+                    else:
+                        clipboard_result = "and it has not been copied to the clipboard"
 
-                if copy_to_clipboard.lower() == "y":
-                    pyperclip.copy(str(password))
-                    clipboard_result = "and it has been copied to the clipboard"
+                # If show_password setting is false run this block
                 else:
-                    clipboard_result = "and it has not been copied to the clipboard"
+                    hidden_password = "*" * password_length
+                    print()
+                    print("Your password:", hidden_password)
+                    pyperclip.copy(str(password))
+                    print()
+                    print("The password has been automatically copied to the clipboard because the security setting "
+                          "in the config.ini file hides the display of the password.")
+                    clipboard_result = "and it has been copied to the clipboard"
 
                 write_to_log(password_length, clipboard_result, password, config)
-
+                print()
                 input("Press Enter to exit.")
                 break
 
         except ValueError:
+            print()
             print('Invalid format, non-numeric characters entered.')
             print()
         except KeyboardInterrupt:
+
             if enable_logging:
                 log_filename = config.get('log', 'filename')
                 logging.getLogger().handlers = []
@@ -130,5 +159,6 @@ def main():
             raise
 
 
+# Main app starts here
 if __name__ == "__main__":
     main()
